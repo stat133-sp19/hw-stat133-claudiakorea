e774bb6d37b38a13fdfc966d7bb7640f7abdfc10
Claudia Korea <16626982+claudiakorea@users.noreply.github.com>
2019-04-17 23:57:53 -0700

    h

--- workout02/app.R
+++ workout02/app.R
@@ -40,7 +40,7 @@ ui <- fluidPage(
                      min = 1,
                      max = 20,
                      value = 2),
-         selectInput("facet", "Facet?", c("Yes", "No"))
+         selectInput("facet", "Facet?", c("No", "Yes"))
       ),
       
       # Show a plot of the generated distribution
@@ -60,32 +60,75 @@ server <- function(input, output) {
   
   
    output$timeline <- renderPlot({
-     modalities <- data.frame(
-       years = 0: input$years,
-       no_contrib = rep(input$initial, input$years + 1),
-       fixed_contrib = rep(input$initial, input$years + 1),
-       growing_contrib = rep(input$initial, input$years + 1)
-     )
-
-     for(y in 1:input$years){
+     if (input$facet == "No"){
+       modalities <- data.frame(
+         years = 0: input$years,
+         no_contrib = rep(input$initial, input$years + 1),
+         fixed_contrib = rep(input$initial, input$years + 1),
+         growing_contrib = rep(input$initial, input$years + 1)
+       )
+  
+       for(y in 1:input$years){
+         rate <- input$rate/100
+         growth <- input$growth/100
+         fv <- input$initial * (1 + rate)** y
+         fva <- input$contrib * ((1 + rate)**y - 1) / rate
+        fvga <- input$contrib * ((1 + rate)**y - (1 + growth)**y) / (rate - growth)
+         modalities$no_contrib[y + 1] <- fv
+         modalities$fixed_contrib[y + 1] <- fv + fva
+         modalities$growing_contrib[y + 1] <- fv + fvga
+       }
+       
+        ggplot(modalities, aes(years)) + geom_line(aes(y = no_contrib, color = 'no_contrib'))+
+          geom_line(aes(y = fixed_contrib, color = "fixed_contrib")) + geom_line(aes(y = growing_contrib, color = "growing_contrib")) +  
+          geom_point(aes(y = fixed_contrib, color = "fixed_contrib")) + geom_point(aes(y = growing_contrib, color = "growing_contrib")) +  
+          geom_point(aes(y = no_contrib, color = 'no_contrib'))+
+          ggtitle("Three modes of investing")
+     }
+     else{
+       investments <- c(
+         'regular savings',
+         'high-yield savings',
+         'index fund')
+       rates <- list(
+         regular = 0.0001,
+         high_yield = 0.0225,
+         index_fund = 0.065
+         
+       )
+       
+       scenarios <- list(1:3)
        rate <- input$rate/100
        growth <- input$growth/100
-       fv <- input$initial * (1 + rate)** y
-       fva <- input$contrib * ((1 + rate)**y - 1) / rate
-      fvga <- input$contrib * ((1 + rate)**y - (1 + growth)**y) / (rate - growth)
-       modalities$no_contrib[y + 1] <- fv
-       modalities$fixed_contrib[y + 1] <- fv + fva
-       modalities$growing_contrib[y + 1] <- fv + fvga
+       for (r in 1:length(rates)){
+         balances <- data.frame(
+           year = 0:input$years,
+           product = investments[r],
+           no_contrib = rep(input$initial, input$years + 1),
+           fixed_contrib = rep(input$initial, input$years + 1),
+           growing_contrib = rep(input$initial, input$years + 1)
+         )
+         for (y in 1:input$years){
+           
+           fv <- input$initial * (1 + rates[[r]])** y
+           fva <- input$contrib * ((1 + rates[[r]])**y - 1) / rates[[r]]
+           fvga <- input$contrib * ((1 + rates[[r]])**y - (1 + growth)**y) / (rates[[r]] - growth)
+           balances$no_contrib[y + 1] <- fv
+           balances$fixed_contrib[y + 1] <- fv + fva
+           balances$growing_contrib[y + 1] <- fv + fvga 
+         }
+         scenarios[[r]] <- balances
+       }
+      dat <- do.call("rbind", scenarios)
+      
+      ggplot(dat, aes(x = year)) 
+      
      }
-      ggplot(modalities, aes(years)) + geom_line(aes(y = no_contrib, color = 'no_contrib'))+
-        geom_line(aes(y = fixed_contrib, color = "fixed_contrib")) + geom_line(aes(y = growing_contrib, color = "growing_contrib")) +  
-        geom_point(aes(y = fixed_contrib, color = "fixed_contrib")) + geom_point(aes(y = growing_contrib, color = "growing_contrib")) +  
-        geom_point(aes(y = no_contrib, color = 'no_contrib'))+
-        ggtitle("Three modes of investing")
    })
    
    output$table <- renderTable(
      {
+       if (input$facet == "No"){
        modalities <- data.frame(
          years = 0: input$years,
          no_contrib = rep(input$initial, input$years + 1),
@@ -103,6 +146,44 @@ server <- function(input, output) {
        }
        
        return(modalities)
+       }
+       else{
+         investments <- c(
+           'regular savings',
+           'high-yield savings',
+           'index fund')
+         rates <- list(
+           regular = 0.0001,
+           high_yield = 0.0225,
+           index_fund = 0.065
+           
+         )
+         
+         scenarios <- list(1:3)
+         rate <- input$rate/100
+         growth <- input$growth/100
+         for (r in 1:length(rates)){
+           balances <- data.frame(
+             year = 0:input$years,
+             product = investments[r],
+             no_contrib = rep(input$initial, input$years + 1),
+             fixed_contrib = rep(input$initial, input$years + 1),
+             growing_contrib = rep(input$initial, input$years + 1)
+           )
+           for (y in 1:input$years){
+             
+             fv <- input$initial * (1 + rates[[r]])** y
+             fva <- input$contrib * ((1 + rates[[r]])**y - 1) / rates[[r]]
+             fvga <- input$contrib * ((1 + rates[[r]])**y - (1 + growth)**y) / (rates[[r]] - growth)
+             balances$no_contrib[y + 1] <- fv
+             balances$fixed_contrib[y + 1] <- fv + fva
+             balances$growing_contrib[y + 1] <- fv + fvga 
+           }
+           scenarios[[r]] <- balances
+         }
+         dat <- do.call("rbind", scenarios)
+         return(dat)
+       }
        
      }
    )
